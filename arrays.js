76f3598a4383bddc8ca20defb669480c32e82722
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-10-26 15:05:22 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+chocolateBars=['snickers','hundred grand','kitkat','skittles'];
+
+function addElementToBeginningOfArray(array,element){
+    var newArray= array.unshift(element);
+    // return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  return array.unshift(element);
+
+}
+
+function addElementToEndOfArray(array,element){
+  var newArray=array.push(element);
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.push(element);
+}
